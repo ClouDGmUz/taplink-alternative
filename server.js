ec73f0a4ccb41d430b8a7d88eb613721805a5a15
ClouDGmUz <cloudgmuz@mgial.com>
2025-03-05 13:44:50 +0500

    maded some changes

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err)
+  process.exit(1)
+})
+
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Rejection:', err)
+  process.exit(1)
+})
+
+let server
+
 app.prepare().then(() => {
-  const server = require('http').createServer((req, res) => {
+  server = require('http').createServer((req, res) => {
     handle(req, res)
   })
 
@@ -13,4 +25,20 @@ app.prepare().then(() => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${process.env.PORT}`)
   })
+
+  process.on('SIGTERM', gracefulShutdown)
+  process.on('SIGINT', gracefulShutdown)
 })
+
+function gracefulShutdown() {
+  console.log('Received kill signal, shutting down gracefully')
+  server.close(() => {
+    console.log('Closed out remaining connections')
+    process.exit(0)
+  })
+
+  setTimeout(() => {
+    console.error('Could not close connections in time, forcefully shutting down')
+    process.exit(1)
+  }, 10000)
+}
